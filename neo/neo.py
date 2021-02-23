814ade96f9f63ad9a99267eb6a96a1c38822a9b8
Giovanni Burresi <giovanniburresi@gmail.com>
2021-02-23 22:11:58 +0100

    Update neo.py

--- neo/neo.py
+++ neo/neo.py
@@ -1,16 +1,12 @@
 import smbus
 import time
 
-INPUT = 'in'
-OUTPUT = 'out'
-HIGH = '1'
-LOW = '0'
 
-PATH_ADC_HOST = '/sys/bus/iio/devices/'
+PATH_ADC_HOST      = '/sys/bus/iio/devices/'
 PATH_ADC_CONTAINER = '/var/adc/'
-PATH_ADC = PATH_ADC_HOST
+PATH_ADC           = PATH_ADC_HOST
 
-ADC_FOLDER = PATH_ADC
+ADC_FOLDER         = PATH_ADC
 
 A0 = ADC_FOLDER + 'iio:device0/' + 'in_voltage0_raw'
 A1 = ADC_FOLDER + 'iio:device0/' + 'in_voltage1_raw'
@@ -19,20 +15,27 @@ A3 = ADC_FOLDER + 'iio:device0/' + 'in_voltage3_raw'
 A4 = ADC_FOLDER + 'iio:device1/' + 'in_voltage0_raw'
 A5 = ADC_FOLDER + 'iio:device1/' + 'in_voltage1_raw'
 
+DEFAULT_BURST_SIZE = 1024;
+
+
+INPUT  = 'in'
+OUTPUT = 'out'
+HIGH   = '1'
+LOW    = '0'
 
 gpios = [ "178", "179", "104", "143", "142", "141", "140", "149", #J4in
-                "105", "148", "146", "147", "100", "102", #J6 in
-                "106", "107", "180", "181", "172", "173", "182", "124", #J4 out
-                "25",  "22",  "14",  "15",  "16",  "17", "18",   "19",  "20",  "21",
-                "203", "202", "177", "176", "175", "174",
-                "119", "124", "127", "116",   "7",   "6",   "5",   "4"]
+          "105", "148", "146", "147", "100", "102", #J6 in
+          "106", "107", "180", "181", "172", "173", "182", "124", #J4 out
+          "25",  "22",  "14",  "15",  "16",  "17",  "18",   "19",  "20",  "21",
+          "203", "202", "177", "176", "175", "174",
+          "119", "124", "127", "116",   "7",   "6",   "5",   "4"]
 
 gpios_J4_in  = { '0'  : 178, '1'  : 179, '2'  : 104, '3'  : 143, '4'  : 142, '5'  : 141, '6'  : 140, '7'  : 149 }
 gpios_J4_out = { '16' : 106, '17' : 107, '18' : 180, '19' : 181, '20' : 172, '21' : 173, '22' : 182, '23' : 124 }
 gpios_J6_in  = { '8'  : 105, '9'  : 148, '10' : 146, '11' : 147, '12' : 100, '13' : 102 }
-gpios_J6_out = { '24' : 25,  '25' : 22,  '26' : 14,  '27' : 15,  '28' : 16,  '29' : 17, '30' : 18,   '31' : 19, '32' : 20, '33' : 21 }
+gpios_J6_out = { '24' : 25,  '25' : 22,  '26' : 14,  '27' : 15,  '28' : 16,  '29' : 17,  '30' : 18,  '31' : 19, '32' : 20, '33' : 21 }
 gpios_J5_out = { '34' : 203, '35' : 202, '36' : 177, '37' : 176, '38' : 175, '39' : 174 }
-gpios_J7_out = { '40' : 119, '41' : 124, '42' : 127, '43' : 116, '44' : 7,   '45' : 6, '46' : 5, '47' : 4 }
+gpios_J7_out = { '40' : 119, '41' : 124, '42' : 127, '43' : 116, '44' : 7,   '45' : 6,   '46' : 5,   '47' : 4 }
 
 gpios_dict = dict()
 gpios_dict.update(gpios_J4_in)
@@ -45,16 +48,15 @@ gpios_dict.update(gpios_J7_out)
 base_path = "/sys/class/gpio"
 
 
-LM75_ADDRESS = 0x48
-LM75_TEMP_REGISTER = 0
-LM75_CONF_REGISTER = 1
-LM75_THYST_REGISTER = 2
-LM75_TOS_REGISTER = 3
-LM75_CONF_SHUTDOWN = 0
+LM75_ADDRESS          = 0x48
+LM75_TEMP_REGISTER    = 0
+LM75_CONF_REGISTER    = 1
+LM75_THYST_REGISTER   = 2
+LM75_TOS_REGISTER     = 3
+LM75_CONF_SHUTDOWN    = 0
 LM75_CONF_OS_COMP_INT = 1
-LM75_CONF_OS_POL = 2
-LM75_CONF_OS_F_QUE = 3
-
+LM75_CONF_OS_POL      = 2
+LM75_CONF_OS_F_QUE    = 3
 
 
 class TemperatureBrick(object):
@@ -92,7 +94,6 @@ class LightBrick(object):
         time.sleep(0.5)
 
 
-
     def getFullSpectrum(self):
         data = self._bus.read_i2c_block_data(self._address, 0x0C | 0x80, 2)
         data1 = self._bus.read_i2c_block_data(self._address, 0x0E | 0x80, 2)
@@ -104,7 +105,6 @@ class LightBrick(object):
         return ch0
 
 
-
     def getInfraredSpectrum(self):
         data = self._bus.read_i2c_block_data(self._address, 0x0C | 0x80, 2)
         data1 = self._bus.read_i2c_block_data(self._address, 0x0E | 0x80, 2)
@@ -114,7 +114,6 @@ class LightBrick(object):
         ch1 = data1[1] * 256 + data1[0]
 
         return ch1
-
 
 
     def getVisibleSpectrum(self):
@@ -131,6 +130,7 @@ class LightBrick(object):
 
 
 class BarometricBrick(object):
+
     def __init__(self, address=0x60, busnum=1):
         self._address = address
         self._bus = smbus.SMBus(busnum)
@@ -183,18 +183,10 @@ class BarometricBrick(object):
         return pressure
 
 
-
-
-
-
-class UdooNeo( object ):
+class Gpio(object):
 
     def __init__(self):
-        self.light_brick = LightBrick()
-        self.temperature_brick = TemperatureBrick()
-        self.barometric_brick = BarometricBrick()
-        print( "Neo lib initialized..." )
-
+        print("gpiosss")
 
     def export_gpio(self, pin):
         gpio = str( gpios_dict[str(pin)] )
@@ -224,13 +216,6 @@ class UdooNeo( object ):
     def unexport_all_gpios(self):
         for pin in gpios_dict.keys():
             self.unexport_gpio( pin )
-
-
-    def export(self, pin, mode):
-        return 0
-
-    def unexport(self, pin):
-        return 0
 
 
     def pinMode(self, pin, direction):
@@ -265,50 +250,8 @@ class UdooNeo( object ):
       return value
 
 
-    #################### ANALOG ##############################3
-    PATH_ADC_HOST = '/sys/bus/iio/devices/'
-    PATH_ADC_CONTAINER = '/var/adc/'
-    PATH_ADC = PATH_ADC_HOST
+class Pwm(object):
 
-    '''
-    A0 =  '/sys/bus/iio/devices/iio\:device0/in_voltage0_raw'
-    A1 =  '/sys/bus/iio/devices/iio\:device0/in_voltage1_raw'
-    A2 =  '/sys/bus/iio/devices/iio\:device0/in_voltage2_raw'
-    A3 =  '/sys/bus/iio/devices/iio\:device0/in_voltage3_raw'
-    A4 =  '/sys/bus/iio/devices/iio\:device1/in_voltage0_raw'
-    A5 =  '/sys/bus/iio/devices/iio\:device1/in_voltage1_raw'
-    '''
-
-    ADC_FOLDER = PATH_ADC
-
-    A0 = ADC_FOLDER + 'iio:device0/' + 'in_voltage0_raw'
-    A1 = ADC_FOLDER + 'iio:device0/' + 'in_voltage1_raw'
-    A2 = ADC_FOLDER + 'iio:device0/' + 'in_voltage2_raw'
-    A3 = ADC_FOLDER + 'iio:device0/' + 'in_voltage3_raw'
-    A4 = ADC_FOLDER + 'iio:device1/' + 'in_voltage0_raw'
-    A5 = ADC_FOLDER + 'iio:device1/' + 'in_voltage1_raw'
-
-
-    DEFAULT_BURST_SIZE = 1024;
-
-    def readAdc(self, pinPath):
-        f = open(pinPath, 'r')
-        data = int(f.read())
-        f.close()
-        return data
-
-    def readBurst(self, pinPath, size=DEFAULT_BURST_SIZE):
-        data = list()
-        for i in range( 0, int(size) ):
-            data.append( readAdc(pinPath) )
-        return data
-
-    def analogRead(self, pin):
-        return self.readAdc(pin)
-
-
-
-    ########### PWM ########################
     pwm_base_path = "/sys/class/pwm/"
     pwm_pins = [3, 4, 5, 6, 7, 9, 11, 10] # from pwm_1 to 8
     pwm_dict = {"3" : (0, 0), "4" : (0, 1), "5" : (0, 2), "6" : (0, 3), "7" : (1, 0), "9" : (1, 1), "11" : (1, 2), "10" : (1, 3)  }
@@ -320,7 +263,7 @@ class UdooNeo( object ):
         return 0
 
     def analogWrite(self, pin, duty_cycle, period=100 , enable=1):
-        bank, num = pwm_dict[ str(pin) ]
+        bank, num = self.pwm_dict[ str(pin) ]
         try:
             with open(self.pwm_base_path + "/export", "w") as pwm:
                 pwm.write(str( bank * 4 + num ))
@@ -347,8 +290,90 @@ class UdooNeo( object ):
             print(e)
 
 
+class Analogs(object):
 
-    ######################## CPU #################################
+    def readAdc(self, pinPath):
+        f = open(pinPath, 'r')
+        data = int(f.read())
+        f.close()
+        return data
+
+    def readBurst(self, pinPath, size=DEFAULT_BURST_SIZE):
+        data = list()
+        for i in range( 0, int(size) ):
+            data.append( readAdc(pinPath) )
+        return data
+
+    def analogRead(self, pin):
+        return self.readAdc(pin)
+
+
+class Accelerometer(object):
+
+    def __init__(self):
+        print("accelerometer")
+
+    def getData(self):
+        print("get data")
+
+    def enable(self):
+        print("en")
+
+    def disable(self):
+        print("dis")
+
+
+class Magnetometer(object):
+
+    def __init__(self):
+        print("magnetometer")
+
+    def getData(self):
+        print("get data")
+
+    def enable(self):
+        print("en")
+
+    def disable(self):
+        print("dis")
+
+
+class Gyroscope(object):
+
+    def __init__(self):
+        print("gyroscope")
+
+    def getData(self):
+        print("get data")
+
+    def enable(self):
+        print("en")
+
+    def disable(self):
+        print("dis")
+
+
+
+
+class UdooNeo( object ):
+
+    def __init__(self):
+        self.light_brick = LightBrick()
+        self.temperature_brick = TemperatureBrick()
+        self.barometric_brick = BarometricBrick()
+        self.gpio = Gpio()
+        self.pwm = Pwm()
+        self.analog = Analogs()
+        print( "Neo lib initialized..." )
+
+
+    def export(self, pin, mode):
+        return 0
+
+    def unexport(self, pin):
+        return 0
+
+
     def getBoardId(self):
         try:
             with open("/sys/fsl_otp/HW_OCOTP_CFG0", "r") as reader:
